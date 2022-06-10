6af40cff66f7333d303bf1555316fc495f76a25b
Beefr <corentin.renault@telecom-st-etienne.fr>
2022-06-10 10:13:57 +0000

    bugfix

--- app/main.py
+++ app/main.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def Menu():
-	if request.method == 'GET':
-        	return m.Menu().showMenu("")
-    	if request.method == 'POST':
-        	user_input = request.form("user_input")
-        	return return m.Menu().showMenu(user_input)
-	#return m.Menu().showMenu()
+    if request.method == 'GET':
+        return m.Menu().showMenu("")
+    if request.method == 'POST':
+        user_input = request.form("user_input")
+        return m.Menu().showMenu(user_input)
+    #return m.Menu().showMenu()
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
