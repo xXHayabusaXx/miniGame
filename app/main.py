97c03738420b1430b8b26227fb7c716ac3e64ab5
Beefr <corentin.renault@telecom-st-etienne.fr>
2022-06-15 08:51:45 +0000

    seems like disabling the reloader makes it keep track of the app state

--- app/main.py
+++ app/main.py
@@ -26,4 +26,4 @@ def clean():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=8080, debug=True)#, use_reloader=False)
