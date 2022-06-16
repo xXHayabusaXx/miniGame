01b5af644735a5426bbf1be051c5d4929392a0db
Beefr <corentin.renault@telecom-st-etienne.fr>
2022-06-16 08:44:27 +0000

    new route to see bdd

--- app/main.py
+++ app/main.py
@@ -34,6 +34,10 @@ def clean():
     return redirect('/login/')
 
 
+@app.route("/bdd/", methods=['GET'])
+def bdd():
+    return menu.showBDD()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
