c88218a28ef0a6a5524af6ea03972f23857637dc
Beefr <corentin.renault@telecom-st-etienne.fr>
2022-06-23 11:37:20 +0000

    revert session manager

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ from flask import Flask, request, redirect, session
 import sys
 sys.path.insert(1, 'OnePiece/workspace/python-pipeline/')
 import menu as m
+import sessionManager as sm
+
 
 app = Flask(__name__)
 
-sessionManager=SessionManager()
+sessionManager=sm.SessionManager()
 #menu = m.Menu()
 
 @app.route("/", methods=['GET','POST'])
@@ -55,33 +57,7 @@ if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
     
   
-  
-class SessionManager(object):
-    
-    
-    def __init__(self):
-        self._playersID={}
-        self._menusID={}
-        
-    def newSession(self, username):
-        newid=self.maxID()+1
-        self._playersID[username]=newid
-        self._menusID[newid]=m.Menu()
-        return self._menusID[newid]
-        
-        
-    def session(self, username):
-        if username in self._playersID:
-            return self.getMenu(username)
-        else:
-            return self.newSession(username)
-        
-    def getMenu(self, username):
-        return self._menusID[self._playersID[username]]
-    
-    
-    def maxID():
-        return max(self._playersID.values())
+
     
     
     
