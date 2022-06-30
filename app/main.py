be196cc6e81db05bc99a4a2f60f8e651e20d446b
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-06-30 12:22:01 +0200

    bugfix

--- app/main.py
+++ app/main.py
@@ -33,8 +33,9 @@ def Menu():
             return "Bad request error"
 
         output=sessionManager.session(session.get('username')).showMenu(user_input)
+        
         formulaire= IndexForm()
-        return render_template('index.html', map=output['map'], content=output['content'], team=output['team'], form=formulaire)
+        return render_template('index.html', output, form=formulaire)
         
         
             
