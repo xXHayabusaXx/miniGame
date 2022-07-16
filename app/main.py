d5211406c059b9cc74b40bfb1fc7cd8d1648847f
Beefr <coreno@laposte.net>
2022-07-16 16:48:12 +0200

    trying to fix session duration

--- app/main.py
+++ app/main.py
@@ -39,6 +39,7 @@ def menu(username=None, user_input=None):
     form=IndexForm()
     if form.validate_on_submit():
         user_input=form.user_input
+        return redirect(url_for('menu', username=current_user.username, user_input=user_input))
         
     output = current_user.menu.showMenu(user_input)
     return render_template('index.html', output=output, form=IndexForm(), username=username)
